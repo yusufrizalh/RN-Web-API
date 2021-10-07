46eeaf2e2fab76088d59500fd31b745ee22043e8
Yusuf Rizal <yusufrizalh@gmail.com>
2021-10-07 15:19:38 +0700

    reacr-native access web api

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import PegawaiView from "./pegawai/PegawaiView";
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="PegawaiView">
+        <Stack.Screen
+          name="PegawaiView"
+          component={PegawaiView}
+          options={{
+            title: "Lihat Data Pegawai",
+            headerStyle: { backgroundColor: "#d34a22" },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const App = () => {
+  return (
+    <View>
+      <MyStack />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ pegawai/PegawaiView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const PegawaiView = () => {
+  return (
+    <View>
+      <Text>View Data Pegawai</Text>
+    </View>
+  );
+};
+
+export default PegawaiView;
